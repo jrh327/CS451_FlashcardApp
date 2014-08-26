51a5618a2114a0ba79ead070831af0d91e739e89
Jon Hopkins <jhopkins720@gmail.com>
2014-08-26 00:19:30 -0400

    Added header to Deck.java

--- group8/cs451/drexel/Deck.java
+++ group8/cs451/drexel/Deck.java
@@ -1,3 +1,15 @@
+/**
+ * Deck.java: object for a deck of cards
+ * 
+ * This file is part of FlashcardApp
+ * 
+ * Contributors:
+ * Jon Hopkins
+ * Jesse Kuehn
+ * Rishir Patel
+ * Sanjana Raj
+ */
+
 package group8.cs451.drexel;
 
 import java.util.Vector;
