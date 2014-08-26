312a1e84f470942a8a53666f07c7d68a4436e7a3
Jon Hopkins <jhopkins720@gmail.com>
2014-08-26 00:18:43 -0400

    Added header to Main.java

--- group8/cs451/drexel/Main.java
+++ group8/cs451/drexel/Main.java
@@ -1,3 +1,15 @@
+/**
+ * Main.java: main routine
+ * 
+ * This file is part of FlashcardApp
+ * 
+ * Contributors:
+ * Jon Hopkins
+ * Jesse Kuehn
+ * Rishir Patel
+ * Sanjana Raj
+ */
+
 package group8.cs451.drexel;
 
 public class Main {
