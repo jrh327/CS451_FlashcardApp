fedecc1d02e7c0b501a539c6f5a637e8004fb6f6
jrh327 <jhopkins720@gmail.com>
2014-08-30 00:45:23 -0400

    Added more use-specific examples of SQLiteHandler usage

--- group8/cs451/drexel/Main.java
+++ group8/cs451/drexel/Main.java
@@ -27,24 +27,44 @@ public class Main {
 		}
 		
 		try {
+			// Drop all the tables in case they already exist
+			sqlite.dropTable("Decks");
+			sqlite.dropTable("Cards");
+			sqlite.dropTable("Sides");
+			
+			// Create all the tables
 			sqlite.createTable("Decks", "ID,Name", "INTEGER PRIMARY KEY ASC,CHAR(50) UNIQUE");
 			sqlite.createTable("Cards", "ID,DeckID,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,INTEGER");
 			sqlite.createTable("Sides", "ID,CardID,Label,Text,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,CHAR(50),CHAR(100),INTEGER");
 			
+			// Insert some rows into table Decks
 			sqlite.insert("Decks", "Name", "TestDeck");
 			sqlite.insert("Decks", "Name", "TestDeck2");
-			sqlite.insert("Decks", "Name", "TestDeck3");
 			
-			sqlite.update("Decks", "Name", "Test2", "ID = ?", "2");
+			// Insert some rows into table Cards
+			sqlite.insert("Cards", "DeckID,Weight", "2,60");
+			sqlite.insert("Cards", "DeckID,Weight", "2,50");
+			sqlite.insert("Cards", "DeckID,Weight", "1,10");
 			
-			ArrayList<String> arr = sqlite.selectColumn("Decks", "Name", "", "");
-			for (int i = 0; i < arr.size(); i++) {
-				System.out.println("Got from Table Decks: " + arr.get(i));
-			}
+			// Insert some sides for the first card in the first deck
+			sqlite.insert("Sides", "CardID,Label,Text,Weight", "1,Test Label,Test Text,100");
+			sqlite.insert("Sides", "CardID,Label,Text,Weight", "1,TestLabel,TestText,40");
 			
-			sqlite.delete("Decks", "Name = ?", "TestDeck2");
+			// Insert some sides for the second card in the second deck
+			sqlite.insert("Sides", "CardID,Label,Text,Weight", "2,Chemical Name,Water,50");
+			sqlite.insert("Sides", "CardID,Label,Text,Weight", "2,Chemical Formula, H20,40");
 			
-			ArrayList<ArrayList<String>> rows = sqlite.select("Decks", "*", "", "");
+			// Change the name of deck TestDeck2 to Chemistry
+			sqlite.update("Decks", "Name", "Chemistry", "ID = ?", "2");
+			
+			// Delete deck TestDeck, all its cards, and the sides of those cards
+			sqlite.delete("Sides", "CardId IN (SELECT ID FROM Cards WHERE DeckID = (SELECT ID FROM Decks WHERE Name = ?))", "TestDeck");
+			sqlite.delete("Cards", "DeckID = (SELECT ID FROM Decks WHERE Name = ?)", "TestDeck");
+			sqlite.delete("Decks", "Name = ?", "TestDeck");
+			
+			// Get all the cards, matched with their respective decks
+			// Should only return two cards that are in deck Test2
+			ArrayList<ArrayList<String>> rows = sqlite.select("Decks d, Cards c", "d.Name, c.Weight", "c.DeckID = d.ID", "");
 			for (int i = 0; i < rows.size(); i++) {
 				ArrayList<String> row = rows.get(i);
 				System.out.print("Row " + i + ": ");
@@ -63,4 +83,5 @@ public class Main {
 			sqlite.close();
 		}
 	}
+	
 }
