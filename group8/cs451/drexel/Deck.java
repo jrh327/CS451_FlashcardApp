167a587d0ebff899c9bb47d5c44fda7c278b5d09
jrh327 <jhopkins720@gmail.com>
2014-08-30 02:57:08 -0400

    Added id and dirty flag to Deck object

--- group8/cs451/drexel/Deck.java
+++ group8/cs451/drexel/Deck.java
@@ -15,22 +15,31 @@ package group8.cs451.drexel;
 import java.util.Vector;
 
 public class Deck {
+	private int id;
 	private String name;
 	private Vector<Flashcard> cards;
+	private boolean isDirty = false;
 	
-	public Deck() {
+	public Deck(int id) {
+		this.id = id;
 		this.name = "";
 		this.cards = new Vector<Flashcard>();
 	}
 	
-	public Deck(String name) {
+	public Deck(int id, String name) {
+		this.id = id;
 		this.name = name;
 		this.cards = new Vector<Flashcard>();
 	}
 	
-	public Deck(String name, Vector<Flashcard> cards) {
+	public Deck(int id, String name, Vector<Flashcard> cards) {
+		this.id = id;
 		this.name = name;
 		this.cards = copyCards(cards);
+	}
+	
+	public int getID() {
+		return this.id;
 	}
 	
 	public String getName() {
@@ -43,10 +52,30 @@ public class Deck {
 	
 	public void setName(String name) {
 		this.name = name;
+		this.isDirty = true;
 	}
 	
 	public void setCards(Vector<Flashcard> cards) {
 		this.cards = copyCards(cards);
+	}
+	
+	public void addCard(Flashcard card) {
+		if (this.cards.contains(card)) {
+			return;
+		}
+		this.cards.add(card);
+	}
+	
+	public void removeCard(Flashcard card) {
+		this.cards.remove(card);
+	}
+	
+	public boolean isDirty() {
+		return this.isDirty;
+	}
+	
+	public void markDirty() {
+		this.isDirty = true;
 	}
 	
 	private Vector<Flashcard> copyCards(Vector<Flashcard> cards) {
@@ -59,10 +88,26 @@ public class Deck {
 		
 		for (int i = 0; i < numCards; i++) {
 			Flashcard curCard = cards.get(i);
-			Flashcard newCard = new Flashcard(curCard.getSides(), curCard.getWeight());
+			Flashcard newCard = new Flashcard(curCard.getID(), curCard.getSides(), curCard.getWeight());
 			copy.add(newCard);
 		}
 		
 		return copy;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Deck)) {
+			return false;
+		}
+		
+		// assume Object o is equal to this if the names
+		// are the same, because the database requires each
+		// deck's name to be unique, so there should never
+		// be a case where a deck with the same name would
+		// not be the deck we're looking for
+		// check the ids too just in case
+		Deck d = (Deck)o;
+		return (d.getID() == this.id && d.name.equals(this.name));
 	}
 }
