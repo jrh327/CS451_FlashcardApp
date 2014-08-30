d32300d11ee2ef2070f5628c448815243feb69ac
jrh327 <jhopkins720@gmail.com>
2014-08-29 23:29:54 -0400

    Added code to demonstrate full select and delete function of SQLiteHandler

--- group8/cs451/drexel/Main.java
+++ group8/cs451/drexel/Main.java
@@ -31,7 +31,6 @@ public class Main {
 			sqlite.createTable("Cards", "ID,DeckID,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,INTEGER");
 			sqlite.createTable("Sides", "ID,CardID,Label,Text,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,CHAR(50),CHAR(100),INTEGER");
 			
-			
 			sqlite.insert("Decks", "Name", "TestDeck");
 			sqlite.insert("Decks", "Name", "TestDeck2");
 			sqlite.insert("Decks", "Name", "TestDeck3");
@@ -42,6 +41,22 @@ public class Main {
 			for (int i = 0; i < arr.size(); i++) {
 				System.out.println("Got from Table Decks: " + arr.get(i));
 			}
+			
+			sqlite.delete("Decks", "Name = ?", "TestDeck2");
+			
+			ArrayList<ArrayList<String>> rows = sqlite.select("Decks", "*", "", "");
+			for (int i = 0; i < arr.size(); i++) {
+				ArrayList<String> row = rows.get(i);
+				System.out.print("Row " + i + ": ");
+				for (int j = 0; j < row.size(); j++) {
+					if (j > 0) {
+						System.out.print('\t');
+					}
+					System.out.print(row.get(j));
+				}
+				System.out.println();
+			}
+			
 		} catch (SQLiteException e) {
 			e.printStackTrace();
 		} finally {
