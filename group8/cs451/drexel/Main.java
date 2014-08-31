70ec85565cb9750fa0e91b22bcff48a3cb241695
jrh327 <jhopkins720@gmail.com>
2014-08-31 17:03:43 -0400

    Added code to show GUI on startup. Also fixed up some of the database-related code

--- group8/cs451/drexel/Main.java
+++ group8/cs451/drexel/Main.java
@@ -15,20 +15,48 @@ package group8.cs451.drexel;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 import com.almworks.sqlite4java.SQLiteException;
 
 public class Main {
 	private Vector<Deck> decks;
-	SQLiteHandler sql;
-	
-	private static final String DECK_TABLE = "Decks";
-	private static final String CARD_TABLE = "Cards";
-	private static final String SIDE_TABLE = "Sides";
 	
 	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (Exception e) {
+					//System.err.println("Couldn't use system look and feel.");
+				}
+				
+				// Create and set up the window
+				JFrame frame = new JFrame("FlipIt!");
+				
+				frame.setLocationRelativeTo(null);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				
+				// Add content to the window
+				frame.add(new MainScreen());
+				
+				//Display the window
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
+	}
+	
+	public Main() {
+		//*
 		SQLiteHandler sqlite;
 		try {
-			sqlite = new SQLiteHandler("/tmp/flashcarddb");
+			sqlite = new SQLiteHandler(Config.DATABASE);
 		} catch (SQLiteException e) {
 			e.printStackTrace();
 			return;
@@ -36,45 +64,45 @@ public class Main {
 		
 		try {
 			// Drop all the tables in case they already exist
-			sqlite.dropTable(DECK_TABLE);
-			sqlite.dropTable(CARD_TABLE);
-			sqlite.dropTable(SIDE_TABLE);
+			sqlite.dropTable(Config.DECK_TABLE);
+			sqlite.dropTable(Config.CARD_TABLE);
+			sqlite.dropTable(Config.SIDE_TABLE);
 			
 			// Create all the tables
-			sqlite.createTable(DECK_TABLE, "ID,Name", "INTEGER PRIMARY KEY ASC,CHAR(50) UNIQUE");
-			sqlite.createTable(CARD_TABLE, "ID,DeckID,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,INTEGER");
-			sqlite.createTable(SIDE_TABLE, "ID,CardID,Label,Text,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,CHAR(50),CHAR(100),INTEGER");
+			sqlite.createTable(Config.DECK_TABLE, "ID,Name", "INTEGER PRIMARY KEY ASC,CHAR(50) UNIQUE");
+			sqlite.createTable(Config.CARD_TABLE, "ID,DeckID,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,INTEGER");
+			sqlite.createTable(Config.SIDE_TABLE, "ID,CardID,Label,Text,Weight", "INTEGER PRIMARY KEY ASC,INTEGER,CHAR(50),CHAR(100),INTEGER");
 			
 			// Insert some rows into table Decks
-			sqlite.insert(DECK_TABLE, "Name", "TestDeck");
-			sqlite.insert(DECK_TABLE, "Name", "TestDeck2");
+			sqlite.insert(Config.DECK_TABLE, "Name", "TestDeck");
+			sqlite.insert(Config.DECK_TABLE, "Name", "TestDeck2");
 			
 			// Insert some rows into table Cards
-			sqlite.insert(CARD_TABLE, "DeckID,Weight", "2,60");
-			sqlite.insert(CARD_TABLE, "DeckID,Weight", "2,50");
-			sqlite.insert(CARD_TABLE, "DeckID,Weight", "1,10");
+			sqlite.insert(Config.CARD_TABLE, "DeckID,Weight", "2,60");
+			sqlite.insert(Config.CARD_TABLE, "DeckID,Weight", "2,50");
+			sqlite.insert(Config.CARD_TABLE, "DeckID,Weight", "1,10");
 			
 			// Insert some sides for the first card in the first deck
-			sqlite.insert(SIDE_TABLE, "CardID,Label,Text,Weight", "1,Test Label,Test Text,100");
-			sqlite.insert(SIDE_TABLE, "CardID,Label,Text,Weight", "1,TestLabel,TestText,40");
+			sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight", "1,Test Label,Test Text,100");
+			sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight", "1,TestLabel,TestText,40");
 			
 			// Insert some sides for the second card in the second deck
-			sqlite.insert(SIDE_TABLE, "CardID,Label,Text,Weight", "2,Chemical Name,Water,50");
-			sqlite.insert(SIDE_TABLE, "CardID,Label,Text,Weight", "2,Chemical Formula, H20,40");
+			sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight", "2,Chemical Name,Water,50");
+			sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight", "2,Chemical Formula, H20,40");
 			
 			// Change the name of deck TestDeck2 to Chemistry
-			sqlite.update(DECK_TABLE, "Name", "Chemistry", "ID = ?", "2");
+			sqlite.update(Config.DECK_TABLE, "Name", "Chemistry", "ID = ?", "2");
 			
 			// Delete deck TestDeck, all its cards, and the sides of those cards
-			sqlite.delete(SIDE_TABLE,
-				"CardId IN (SELECT ID FROM " + CARD_TABLE + " WHERE DeckID = (SELECT ID FROM " + DECK_TABLE + " WHERE Name = ?))",
+			sqlite.delete(Config.SIDE_TABLE,
+				"CardId IN (SELECT ID FROM " + Config.CARD_TABLE + " WHERE DeckID = (SELECT ID FROM " + Config.DECK_TABLE + " WHERE Name = ?))",
 				"TestDeck");
-			sqlite.delete(CARD_TABLE, "DeckID = (SELECT ID FROM " + DECK_TABLE + " WHERE Name = ?)", "TestDeck");
-			sqlite.delete(DECK_TABLE, "Name = ?", "TestDeck");
+			sqlite.delete(Config.CARD_TABLE, "DeckID = (SELECT ID FROM " + Config.DECK_TABLE + " WHERE Name = ?)", "TestDeck");
+			sqlite.delete(Config.DECK_TABLE, "Name = ?", "TestDeck");
 			
 			// Get all the cards, matched with their respective decks
 			// Should only return two cards that are in deck Test2
-			ArrayList<ArrayList<String>> rows = sqlite.select(DECK_TABLE + " d, " + CARD_TABLE + " c", "d.Name, c.Weight", "c.DeckID = d.ID", "");
+			ArrayList<ArrayList<String>> rows = sqlite.select(Config.DECK_TABLE + " d, " + Config.CARD_TABLE + " c", "d.Name, c.Weight", "c.DeckID = d.ID", "");
 			for (int i = 0; i < rows.size(); i++) {
 				ArrayList<String> row = rows.get(i);
 				System.out.print("Row " + i + ": ");
@@ -92,6 +120,7 @@ public class Main {
 		} finally {
 			sqlite.close();
 		}
+		//*/
 	}
 	
 	/**
@@ -99,8 +128,16 @@ public class Main {
 	 * For performance reasons, empty Deck objects with only the names are loaded
 	 */
 	public void loadDecks() {
+		SQLiteHandler sqlite;
 		try {
-			ArrayList<ArrayList<String>> arr = sql.select(DECK_TABLE, "ID,Name", "", "");
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			ArrayList<ArrayList<String>> arr = sqlite.select(Config.DECK_TABLE, "ID,Name", "", "");
 			
 			for (int i = 0; i < arr.size(); i++) {
 				ArrayList<String> row = arr.get(i);
@@ -108,8 +145,8 @@ public class Main {
 			}
 		} catch (SQLiteException e) {
 			e.printStackTrace();
-			sql.close();
-			System.exit(0);
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -120,10 +157,18 @@ public class Main {
 	 * @param deck The Deck object to be filled out
 	 */
 	public void loadDeck(Deck deck) {
+		SQLiteHandler sqlite;
+		try {
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
 		try {
 			String deckName = deck.getName();
-			ArrayList<ArrayList<String>> arr = sql.select(CARD_TABLE, "ID,Weight",
-						"DeckID = (SELECT ID FROM " + DECK_TABLE + " WHERE Name = ?", deckName);
+			ArrayList<ArrayList<String>> arr = sqlite.select(Config.CARD_TABLE, "ID,Weight",
+						"DeckID = (SELECT ID FROM " + Config.DECK_TABLE + " WHERE Name = ?", deckName);
 			
 			for (int i = 0; i < arr.size(); i++) {
 				ArrayList<String> row = arr.get(i);
@@ -134,8 +179,8 @@ public class Main {
 			}
 		} catch (SQLiteException e) {
 			e.printStackTrace();
-			sql.close();
-			System.exit(0);
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -145,21 +190,29 @@ public class Main {
 	 * @param deck The deck to save
 	 */
 	public void saveDeck(Deck deck) {
+		SQLiteHandler sqlite;
+		try {
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
 		try {
 			if (deck.isDirty()) {
-				sql.update(DECK_TABLE, "Name", deck.getName(), "ID = ?", String.valueOf(deck.getID()));
+				sqlite.update(Config.DECK_TABLE, "Name", deck.getName(), "ID = ?", String.valueOf(deck.getID()));
 			}
 			Vector<Flashcard> cards = deck.getCards();
 			for (int i = 0; i < cards.size(); i++) {
 				Flashcard card = cards.get(i);
 				if (card.isDirty()) {
-					sql.update(CARD_TABLE, "Weight", String.valueOf(card.getWeight()), "ID = ?", String.valueOf(card.getID()));
+					sqlite.update(Config.CARD_TABLE, "Weight", String.valueOf(card.getWeight()), "ID = ?", String.valueOf(card.getID()));
 					
 					Vector<FlashcardSide> sides = card.getSides();
 					for (int j = 0; j < sides.size(); j++) {
 						FlashcardSide side = sides.get(j);
 						if (side.isDirty()) {
-							sql.update(SIDE_TABLE, "Label,Text,Weight",
+							sqlite.update(Config.SIDE_TABLE, "Label,Text,Weight",
 									side.getLabel() + "," + side.getText() + "," + side.getWeight(),
 									"CardID = ?", String.valueOf(card.getID()));
 						}
@@ -169,10 +222,9 @@ public class Main {
 			
 		} catch (SQLiteException e) {
 			e.printStackTrace();
-			return;
+		} finally {
+			sqlite.close();
 		}
-		
-		
 	}
 	
 	/**
@@ -181,14 +233,24 @@ public class Main {
 	 * @param deckName The name of the new deck
 	 */
 	public void addDeck(String deckName) {
+		SQLiteHandler sqlite;
 		try {
-			sql.insert(DECK_TABLE, "Name", deckName);
-			int id = Integer.parseInt(sql.selectSingle("SELECT MAX(ID) FROM " + DECK_TABLE, ""));
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			sqlite.insert(Config.DECK_TABLE, "Name", deckName);
+			int id = Integer.parseInt(sqlite.selectSingle("SELECT MAX(ID) FROM " + Config.DECK_TABLE, ""));
 			Deck deck = new Deck(id, deckName);
 			deck.markDirty();
 			decks.add(deck);
 		} catch (SQLiteException e) {
 			e.printStackTrace();
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -200,11 +262,19 @@ public class Main {
 	 * @param deck The deck to remove
 	 */
 	public void removeDeck(Deck deck) {
+		SQLiteHandler sqlite;
+		try {
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
 		try {
 			String deckID = String.valueOf(deck.getID());
-			sql.delete(SIDE_TABLE, "CardId IN (SELECT ID FROM " + CARD_TABLE + " WHERE DeckID = ?)", deckID);
-			sql.delete(CARD_TABLE, "DeckID = ?", deckID);
-			sql.delete(DECK_TABLE, "ID = ?", deckID);
+			sqlite.delete(Config.SIDE_TABLE, "CardId IN (SELECT ID FROM " + Config.CARD_TABLE + " WHERE DeckID = ?)", deckID);
+			sqlite.delete(Config.CARD_TABLE, "DeckID = ?", deckID);
+			sqlite.delete(Config.DECK_TABLE, "ID = ?", deckID);
 			decks.remove(new Deck(deck.getID(), deck.getName()));
 		} catch (SQLiteException e) {
 			e.printStackTrace();
@@ -218,8 +288,16 @@ public class Main {
 	 * @param card The card to add
 	 */
 	public void addCardToDeck(Deck deck, Flashcard card) {
+		SQLiteHandler sqlite;
 		try {
-			sql.insert(CARD_TABLE, "DeckID,Weight", card.getID() + "," + card.getWeight());
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			sqlite.insert(Config.CARD_TABLE, "DeckID,Weight", card.getID() + "," + card.getWeight());
 			card.markDirty();
 			deck.addCard(card);
 			
@@ -228,13 +306,14 @@ public class Main {
 				int cardID = card.getID();
 				for (int i = 0; i < sides.size(); i++) {
 					FlashcardSide side = sides.get(i);
-					sql.insert(SIDE_TABLE, "CardID,Label,Text,Weight",
+					sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight",
 							cardID + "," + side.getLabel() + "," + side.getText() + "," + side.getWeight());
 				}
 			}
 		} catch (SQLiteException e) {
 			e.printStackTrace();
-			return;
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -247,12 +326,22 @@ public class Main {
 	 * @param card The card to be removed
 	 */
 	public void removeCardFromDeck(Deck deck, Flashcard card) {
+		SQLiteHandler sqlite;
 		try {
-			sql.delete(SIDE_TABLE, "CardId = ?", String.valueOf(card.getID()));
-			sql.delete(CARD_TABLE, "ID = ?", String.valueOf(card.getID()));
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			sqlite.delete(Config.SIDE_TABLE, "CardId = ?", String.valueOf(card.getID()));
+			sqlite.delete(Config.CARD_TABLE, "ID = ?", String.valueOf(card.getID()));
 			deck.removeCard(card);
 		} catch (SQLiteException e) {
 			e.printStackTrace();
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -263,14 +352,23 @@ public class Main {
 	 * @param side The side to be added
 	 */
 	public void addSideToCard(Flashcard card, FlashcardSide side) {
+		SQLiteHandler sqlite;
 		try {
-			sql.insert(SIDE_TABLE, "CardID,Label,Text,Weight",
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			sqlite.insert(Config.SIDE_TABLE, "CardID,Label,Text,Weight",
 					card.getID() + "," + side.getLabel() + ","  + side.getText() + "," + side.getWeight());
 			side.markDirty();
 			card.addSide(side);
 		} catch (SQLiteException e) {
 			e.printStackTrace();
-			return;
+		} finally {
+			sqlite.close();
 		}
 	}
 	
@@ -283,11 +381,21 @@ public class Main {
 	 * @param side The side to be remove
 	 */
 	public void removeSideFromCard(Flashcard card, FlashcardSide side) {
+		SQLiteHandler sqlite;
 		try {
-			sql.delete(SIDE_TABLE, "ID = ?", String.valueOf(side.getID()));
+			sqlite = new SQLiteHandler(Config.DATABASE);
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			sqlite.delete(Config.SIDE_TABLE, "ID = ?", String.valueOf(side.getID()));
 			card.removeSide(side);
 		} catch (SQLiteException e) {
 			e.printStackTrace();
+		} finally {
+			sqlite.close();
 		}
 	}
 }
