667c71141ffb63355fe42de024be08bc3ad05022
jrh327 <jhopkins720@gmail.com>
2014-08-31 16:57:33 -0400

    Added a file to hold configuration info and common constants

---
+++ group8/cs451/drexel/Config.java
@@ -0,0 +1,20 @@
+/**
+ * Config.java: contains configuration information and constants
+ * 
+ * This file is part of FlashcardApp
+ * 
+ * Contributors:
+ * Jon Hopkins
+ * Jesse Kuehn
+ * Rishir Patel
+ * Sanjana Raj
+ */
+
+package group8.cs451.drexel;
+
+public class Config {
+	public static final String DATABASE = "/tmp/flashcarddb";
+	public static final String DECK_TABLE = "Decks";
+	public static final String CARD_TABLE = "Cards";
+	public static final String SIDE_TABLE = "Sides";
+}
